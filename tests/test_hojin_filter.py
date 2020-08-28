51903c3d752a07802541f4de7604f1df1f5f120c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-08-28 23:53:36 +0900

    make test parmeterized

--- tests/test_hojin_filter.py
+++ tests/test_hojin_filter.py
@@ -6,33 +6,17 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from jawiki.hojin import hojin_filter
 
-
-def test_hojin_filter():
-    kanji, yomi = hojin_filter('愛知県地方木材株式会社', 'あいちけんちほうもくざいかぶしきがいしゃ')
-    assert kanji == '愛知県地方木材'
-    assert yomi == 'あいちけんちほうもくざい'
-
-def test_hojin_filter2():
-    kanji, yomi = hojin_filter('愛知県地方木材株式会社', 'あいちけんちほうもくざい')
-    assert kanji == '愛知県地方木材'
-    assert yomi == 'あいちけんちほうもくざい'
-
-
-def test_hojin_filter3():
-    assert [hojin_filter('株式会社少年画報社', 'しょうねんがほうしゃ')] == \
-            [('少年画報社', 'しょうねんがほうしゃ')]
-    assert [hojin_filter('京浜急行電鉄株式会社', 'けいひんきゅうこうでんてつ')] == \
-            [('京浜急行電鉄', 'けいひんきゅうこうでんてつ')]
-
-def test_hojin_filter4():
-    assert [hojin_filter('アイパック', 'あいぱっくかぶしきかいしゃ')] == \
-            [('アイパック', 'あいぱっく')]
-
-def test_hojin_filter5():
-    assert [hojin_filter('国立研究開発法人宇宙航空研究開発機構', 'うちゅうこうくうけんきゅうかいはつきこう')] == \
-            [('宇宙航空研究開発機構', 'うちゅうこうくうけんきゅうかいはつきこう')]
-
-def test_hojin_filter6():
-    assert [hojin_filter('国立研究開発法人', 'こくりつけんきゅうかいはつほうじん')] == \
-            [('国立研究開発法人', 'こくりつけんきゅうかいはつほうじん')]
+@pytest.mark.parametrize("kanji,kana,expected_kanji,expected_kana", [
+    ('愛知県地方木材株式会社', 'あいちけんちほうもくざいかぶしきがいしゃ', '愛知県地方木材', 'あいちけんちほうもくざい'),
+    ('愛知県地方木材株式会社', 'あいちけんちほうもくざい', '愛知県地方木材', 'あいちけんちほうもくざい'),
+    ('株式会社少年画報社', 'しょうねんがほうしゃ', '少年画報社', 'しょうねんがほうしゃ'),
+    ('京浜急行電鉄株式会社', 'けいひんきゅうこうでんてつ', '京浜急行電鉄', 'けいひんきゅうこうでんてつ'),
+    ('アイパック', 'あいぱっくかぶしきかいしゃ', 'アイパック', 'あいぱっく'),
+    ('国立研究開発法人宇宙航空研究開発機構', 'うちゅうこうくうけんきゅうかいはつきこう', '宇宙航空研究開発機構', 'うちゅうこうくうけんきゅうかいはつきこう'),
+    ('国立研究開発法人', 'こくりつけんきゅうかいはつほうじん', '国立研究開発法人', 'こくりつけんきゅうかいはつほうじん'),
+])
+def test_hojin_filter(kanji, kana, expected_kanji, expected_kana):
+    kanji, yomi = hojin_filter(kanji, kana)
+    assert kanji == expected_kanji
+    assert yomi == expected_kana
 
